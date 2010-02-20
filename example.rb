bedb787df5fad0d487c3a4952158e4edd95203f0
Andrew Carpenter <acarpen@wested.org>
2010-02-20 09:38:10 -0800

    complete example

--- example.rb
+++ example.rb
@@ -3,22 +3,46 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'lib'))
 require 'rubygems'
 require 'gwt_rpc'
 
-class RegulationsDotGovClient < GwtRpc::Client
-  domain 'www.regulations.gov'
-  js_url 'http://www.regulations.gov/search/Regs/'
+module RegulationsDotGov
+  class Metadata
+    def self.gwt_deserialize(reader)
+      empty_hash = reader.read_object
+      sort_info = reader.read_object
+      hash = reader.read_object
+      
+      {hash["metadataName"] => hash["metadataValue"]}
+    end
+  end
   
-  add_procedure :document_details,
-                :path       => '/search/Regs/documentDetail',
-                :identifier => '55D5B79E395B8DDEAC09E3F41C864CA2',
-                :namespace  => 'gov.egov.erule.gwt.module.regs.client.service.DocumentDetailService',
-                :method     => 'getDocumentDetails'
+  class DocumentDetail
+    def self.gwt_deserialize(reader)
+      # pop two objects off the stack that we don't care about
+      reader.read_object
+      reader.read_object 
+      
+      # grab the data we want
+      details = reader.read_object
+      
+      # collapse array of hashes into a single hash
+      details["documentMetadata"] = details["documentMetadata"].inject({}){|base, kv| base.merge!(kv)}
+      
+      details
+    end
+  end
   
-  # add_procedure :search_documents,
-  #               :path       => '/search/Regs/searchResults',
-  #               :identifier => 'AB0B7193CC1148EFEEB8D5771D3EBF33',
-  #               :namespace  => 'gov.egov.erule.gwt.module.regs.client.service.SearchResultsService',
-  #               :method     => 'getSearchResultsByRelevance'
+  class Client < GwtRpc::Client
+    domain 'www.regulations.gov'
+    js_url 'http://www.regulations.gov/search/Regs/'
+  
+    add_procedure :document_details,
+                  :path       => '/search/Regs/documentDetail',
+                  :identifier => '55D5B79E395B8DDEAC09E3F41C864CA2',
+                  :namespace  => 'gov.egov.erule.gwt.module.regs.client.service.DocumentDetailService',
+                  :method     => 'getDocumentDetails'
+  
+    map_classes 'gov.egov.erule.gwt.module.regs.client.model.DocumentDetailModel' => 'RegulationsDotGov::DocumentDetail',
+                'gov.egov.erule.gwt.module.regs.client.model.DocumentMetadataModel' => 'RegulationsDotGov::Metadata'
+  end
 end
-
-client = RegulationsDotGovClient.new
-client.document_details('NOAA-NMFS-2008-0241-0001')
+client = RegulationsDotGov::Client.new
+puts client.document_details('0900006480776e33').inspect
